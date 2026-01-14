9d833295fc5e9c338efce50c67f1baae7c6d7dff
Delon Newman <contact@delonnewman.name>
2026-01-14 17:28:47 -0500

    Add ArrayLike

---
+++ src/wonderscript/lang/ArrayLike.ts
@@ -0,0 +1,10 @@
+import { isNumber } from "./runtime";
+
+export type ArrayLike<T = unknown> = {
+    length: number;
+    [index: number]: T;
+};
+
+export function isArrayLike(val: unknown): val is ArrayLike {
+    return val != null && isNumber((val as ArrayLike).length);
+}
