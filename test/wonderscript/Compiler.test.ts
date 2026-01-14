3c9683c274d362c7e31dd91f42bed028f6c77982
Delon Newman <contact@delonnewman.name>
2026-01-14 14:56:55 -0500

    Fix Compiler test

--- test/wonderscript/Compiler.test.ts
+++ test/wonderscript/Compiler.test.ts
@@ -2,7 +2,7 @@ import { expect, test, describe } from "bun:test";
 import { Compiler } from "../../src/wonderscript";
 
 describe("Compiler", () => {
-  const subject = new Compiler("node", {});
+  const subject = new Compiler("node", { global: {} });
 
   test("slot access", () => {
     let output = subject.evalString('(slot js/global "hey")');
