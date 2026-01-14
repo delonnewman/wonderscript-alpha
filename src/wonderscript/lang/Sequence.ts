ac44992c21b2fd345a67d3d3223410e4ca6e408e
Delon Newman <contact@delonnewman.name>
2026-01-14 17:30:10 -0500

    Add Seq types

--- src/wonderscript/lang/Sequence.ts
+++ src/wonderscript/lang/Sequence.ts
@@ -1,8 +1,31 @@
 import { Nil } from "./Nil";
 import { isFunction } from "./runtime";
+import { ArrayLike } from "./ArrayLike";
 
 export type First<T = unknown> = T | Nil;
-export type Next<T = unknown> = Sequence<T> | Nil;
+export type Next<T = unknown> = Seq<T> | Nil;
+
+type ConsMethod = { cons: (val: unknown) => unknown[] };
+export type Consable = ArrayLike | Nil | ConsMethod;
+
+type HasFirstMethod<T = unknown> = { first: () => T | Nil };
+
+type Firstable<T = unknown, U = unknown> =
+  | HasFirstMethod<T>
+  | ArrayLike<T>
+  | Map<T, U>
+  | Set<T>;
+
+type HasForEachMethod<T = unknown> = {
+  forEach: (cb: (val: T) => void) => void;
+};
+
+type Nextable<T = unknown> = Sequence<T> | ArrayLike<T> | HasForEachMethod<T>;
+
+export type Seq<T = unknown> =
+  | Readonly<T[]>
+  | Sequence<T>
+  | (Firstable<T> & Nextable<T>);
 
 export interface Sequence<T = unknown> {
   cons(val: T): Sequence;
