3e554902b642cd4a8010fa110322aebccaf5688d
Delon Newman <contact@delonnewman.name>
2026-01-14 15:48:43 -0500

    Improve types

--- src/wonderscript/compiler/core.ts
+++ src/wonderscript/compiler/core.ts
@@ -52,7 +52,7 @@ export function isBodyForm<Tag extends BodyTag>(tag: Symbol) {
 }
 
 export function isTaggedValue<Tag = Symbol>(
-  form,
+  form: unknown,
   tag?: Symbol
 ): form is TaggedValue<Tag> {
   const tagged = isArray(form) && isSymbol(form[0]);
@@ -63,14 +63,19 @@ export function isTaggedValue<Tag = Symbol>(
 
 export const RECUR_SYM = Symbol.intern<typeof RECUR_STR>(RECUR_STR);
 
-export function isRecurForm(x): x is TaggedValue<typeof RECUR_SYM> {
-  return isTaggedValue<typeof RECUR_SYM>(x, RECUR_SYM);
+export function isRecurForm(
+  val: unknown
+): val is TaggedValue<typeof RECUR_SYM> {
+  return isTaggedValue<typeof RECUR_SYM>(
+    val as TaggedValue<typeof RECUR_SYM>,
+    RECUR_SYM
+  );
 }
 
 export const THROW_SYM = Symbol.intern<typeof THROW_STR>(THROW_STR);
 
-export function isThrowForm(x): x is TaggedValue {
-  return isTaggedValue(x) && x[0].equals(THROW_SYM);
+export function isThrowForm(val: unknown): val is TaggedValue {
+  return isTaggedValue(val) && val[0].equals(THROW_SYM);
 }
 
 // forms that compile to JS statements
@@ -85,12 +90,12 @@ export function isStatementForm(form: Form): form is StatementForm {
 export const EOF = { eof: true } as const;
 export type EOF = typeof EOF;
 
-export function isEOF(x): x is EOF {
-  return x && x.eof === true;
+export function isEOF(val: unknown): val is EOF {
+  return val && (val as EOF).eof === true;
 }
 
 export type SpecialFormSymbol = keyof typeof SPECIAL_FORMS;
 export type SpecialForm = TaggedValue<Symbol<SpecialFormSymbol>>;
 
-export const isSpecialForm = (value: any): value is SpecialForm =>
+export const isSpecialForm = (value: unknown): value is SpecialForm =>
   isTaggedValue(value) && SPECIAL_FORMS[value[0].name()] === true;
