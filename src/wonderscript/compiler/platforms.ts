1c154956577c08925b12b71a8d159cb7b4f27b1f
Delon Newman <contact@delonnewman.name>
2026-01-15 13:45:50 -0500

    Improve platforms

--- src/wonderscript/compiler/platforms.ts
+++ src/wonderscript/compiler/platforms.ts
@@ -1,6 +1,17 @@
-export type NodeJS = "node";
-export type BunSH = "bun";
-export type PHP = "php";
-export type Browser = "browser";
+const Node = "node";
+const Bun = "bun";
+const PHP = "php";
+const Browser = "browser";
 
-export type Platform = NodeJS | BunSH | Browser | PHP;
+type Node = typeof Node;
+type Bun = typeof Bun;
+type PHP = typeof PHP;
+type Browser = typeof Browser;
+
+export type Platform = Node | Bun | Browser | PHP;
+export const Platform = Object.freeze({
+    Node,
+    Bun,
+    PHP,
+    Browser,
+}) as const;
