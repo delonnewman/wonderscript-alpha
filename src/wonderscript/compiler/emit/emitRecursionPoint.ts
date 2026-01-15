23fa94013abe77d8f2ba5fb94ea43a160a6f8805
Delon Newman <contact@delonnewman.name>
2026-01-14 20:22:35 -0500

    Use string iterpolation

--- src/wonderscript/compiler/emit/emitRecursionPoint.ts
+++ src/wonderscript/compiler/emit/emitRecursionPoint.ts
@@ -1,4 +1,3 @@
-import { str } from "../../lang/runtime";
 import { emit } from "../emit";
 import { RECURSION_POINT_CLASS } from "../constants";
 import { Context } from "../../lang/Context";
@@ -7,5 +6,5 @@ import { TaggedValue } from "../core";
 export function emitRecursionPoint(form: TaggedValue, env: Context): string {
   const args = form.slice(1).map((x) => emit(x, env));
 
-  return str("throw new ", RECURSION_POINT_CLASS, "([", args.join(", "), "])");
+  return `throw new ${RECURSION_POINT_CLASS}([${args.join(", ")}])`;
 }
