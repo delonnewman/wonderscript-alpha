9770f17098503c24903349e4c4c17c3477b4a75f
Delon Newman <contact@delonnewman.name>
2026-01-15 13:53:57 -0500

    Supply a target platform

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { Compiler } = require("./dist/wonderscript.js");
-const ws = new Compiler("node", global);
+const ws = new Compiler("node", "node", global);
 
 if (process.env.NODE_ENV === "production") {
   require("./dist/wonderscript/core.ws.js");
