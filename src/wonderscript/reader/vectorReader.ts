a3ed01a72b3386529b5d629cc22f0e5775396406
Delon R. Newman <contact@delonnewman.name>
2026-01-19 17:02:57 -0500

    Improve types

--- src/wonderscript/reader/vectorReader.ts
+++ src/wonderscript/reader/vectorReader.ts
@@ -2,7 +2,11 @@ import { readDelimitedList } from "./readDelimitedList";
 import { PushBackReader } from "./PushBackReader";
 import { Vector } from "../lang/Vector";
 
-export function vectorReader(r: PushBackReader, openbracket, opts): Vector {
+export function vectorReader(
+  r: PushBackReader,
+  _openbracket: string,
+  opts: Record<string, unknown>
+): Vector {
   const a = readDelimitedList("]", r, true, opts);
 
   return new Vector(a);
