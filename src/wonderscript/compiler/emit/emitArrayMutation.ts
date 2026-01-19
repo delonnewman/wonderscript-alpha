9bc25ea2dfbcd0f605cbd77e8c885ef188b259e4
Delon R. Newman <contact@delonnewman.name>
2026-01-19 12:12:14 -0500

    Use unknown and Context

--- src/wonderscript/compiler/emit/emitArrayMutation.ts
+++ src/wonderscript/compiler/emit/emitArrayMutation.ts
@@ -3,15 +3,16 @@ import { ASET_SYM as ASET_STR } from "../constants";
 import { Form, isTaggedValue } from "../core";
 import { prStr } from "../prStr";
 import { Symbol } from "../../lang/Symbol";
+import { Context } from "../../lang/Context";
 
 export const ASET_SYM = Symbol.intern(ASET_STR);
 
 export type ArrayMutationForm = [typeof ASET_SYM, any[], Form, Form];
 
-export const isArrayMutationForm = (form: Form): form is ArrayMutationForm =>
+export const isArrayMutationForm = (form: unknown): form is ArrayMutationForm =>
   isTaggedValue(form) && form[0].equals(ASET_SYM) && form.length === 4;
 
-export function emitArrayMutation(form, env) {
+export function emitArrayMutation(form: unknown, env: Context) {
   if (!isArrayMutationForm(form))
     throw new Error(`invalid ${ASET_SYM} form: ${prStr(form)}`);
 
