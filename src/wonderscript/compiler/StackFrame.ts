f3bacbaaeeaa4dc65fbb516c7661ed7aab769954
Delon R. Newman <contact@delonnewman.name>
2026-01-19 13:39:40 -0500

    Add StackFrame

---
+++ src/wonderscript/compiler/StackFrame.ts
@@ -0,0 +1,15 @@
+export class StackFrame {
+  readonly file: string
+  readonly line: number
+  readonly column: number
+
+  constructor(file: string, line: number, column: number) {
+    this.file = file;
+    this.line = line;
+    this.column = column;
+  }
+
+  toString() {
+    return `  at ${this.file}:${this.line}:${this.column}`;
+  }
+}
---
+++ test/wonderscript/compiler/StackFrame.test.ts
@@ -0,0 +1,25 @@
+import { expect, test, describe } from "bun:test";
+import { StackFrame } from "../../../src/wonderscript/compiler/StackFrame";
+
+describe("StackFrame", () => {
+  const file   = "test.ws";
+  const line   = 341;
+  const column = 7;
+  const frame  = new StackFrame(file, line, column);
+
+  test("#file", () => {
+    expect(frame.file).toBe(file);
+  });
+
+  test("#line", () => {
+    expect(frame.line).toBe(line);
+  });
+
+  test("#column", () => {
+    expect(frame.column).toBe(column);
+  });
+
+  test("#toString", () => {
+    expect(frame.toString()).toBe(`  at ${file}:${line}:${column}`)
+  });
+});
