2e0b81bdb09f6a1b26e9c2708b4b815f70ecd2ac
Delon R. Newman <contact@delonnewman.name>
2026-01-19 17:00:10 -0500

    Improve types

--- src/wonderscript/reader/listReader.ts
+++ src/wonderscript/reader/listReader.ts
@@ -1,6 +1,10 @@
 import { readDelimitedList } from "./readDelimitedList";
 import { PushBackReader } from "./PushBackReader";
 
-export function listReader(r: PushBackReader, _: "(", opts) {
+export function listReader(
+  r: PushBackReader,
+  _: "(",
+  opts: Record<string, unknown>
+) {
   return readDelimitedList(")", r, true, opts);
 }
