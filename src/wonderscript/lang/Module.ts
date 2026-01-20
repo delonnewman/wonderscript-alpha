1f166b43d0a006681895b2415db6d70aa2eefe20
Delon R. Newman <contact@delonnewman.name>
2026-01-19 20:56:23 -0500

    Import merge from js module

--- src/wonderscript/lang/Module.ts
+++ src/wonderscript/lang/Module.ts
@@ -1,7 +1,7 @@
 import { ALIEN_KW, Definition } from "./Definition";
 import { Symbol } from "./Symbol";
 import { MetaData } from "./Meta";
-import { merge } from "./runtime";
+import { merge } from "../js/merge";
 
 export type DefinitionMap = Map<Symbol, Definition>;
 
