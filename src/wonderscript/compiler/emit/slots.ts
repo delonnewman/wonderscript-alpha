f27fdf7d8e2d78e2c353f935f699de6681b53afd
Delon R. Newman <contact@delonnewman.name>
2026-01-21 21:03:55 -0500

    Don't use isSymbol

--- src/wonderscript/compiler/emit/slots.ts
+++ src/wonderscript/compiler/emit/slots.ts
@@ -1,12 +1,12 @@
 import { isString } from "../../js/predicates";
-import { isKeyword, Keyword } from "../../lang/Keyword";
-import { isSymbol } from "../../lang/Symbol";
+import { isKeyword } from "../../lang/Keyword";
+import { Symbol } from "../../lang/Symbol";
 import { Form, isTaggedValue } from "../core";
 import { escapeChars } from "../utils";
 import { QUOTE_SYM } from "./emitQuote";
 
 export function emitSlotName(slot: Form) {
-  if (isTaggedValue(slot) && slot[0].equals(QUOTE_SYM) && isSymbol(slot[1])) {
+  if (isTaggedValue(slot) && slot[0].equals(QUOTE_SYM) && slot[1] instanceof Symbol) {
     slot = slot[1].name();
   }
 
