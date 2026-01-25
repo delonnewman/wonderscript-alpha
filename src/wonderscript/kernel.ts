f37055a9cc09d08284f936498f4a1465386ac2fe
Delon R. Newman <contact@delonnewman.name>
2026-01-25 16:27:18 -0500

    Add example code

--- src/wonderscript/kernel.ts
+++ src/wonderscript/kernel.ts
@@ -3,3 +3,14 @@ export * from "./kernel/Message";
 export * from "./kernel/Ref";
 export * from "./kernel/Script";
 export * from "./kernel/Symbol";
+
+import { Script } from "./kernel/Script";
+import { Alien } from "./kernel/Alien";
+import { Message } from "./kernel/Message";
+import { Symbol } from "./kernel/Symbol";
+
+const $global = new Alien(globalThis, "globalThis", "js");
+const s = new Script();
+const processRef = s.createReference(Symbol.intern("process"));
+s.send($global, Message.intern("process", { ref: processRef }));
+s.dereference(Symbol.intern("process"));
