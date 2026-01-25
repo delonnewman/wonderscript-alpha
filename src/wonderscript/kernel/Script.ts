0ebb137c709ac9e93f745e342b3fd532df4d4e77
Delon R. Newman <contact@delonnewman.name>
2026-01-25 16:26:18 -0500

    Add references

--- src/wonderscript/kernel/Script.ts
+++ src/wonderscript/kernel/Script.ts
@@ -1,16 +1,30 @@
 import { Statement } from "./Statement";
 import { Message } from "./Message";
 import { Recipient } from "./Dispatch";
+import { Symbol } from "./Symbol";
+import { Ref } from "./Ref";
 
 export class Script {
   #statements: Statement[];
+  #references: Map<string, Ref>;
 
   constructor() {
     this.#statements = [];
+    this.#references = new Map();
   }
 
   get statements () {
     return Object.freeze(this.#statements.slice(0));
+  }
+
+  createReference(name: Symbol, value?: unknown) {
+    const ref = new Ref(value);
+    this.#references.set(name.toString(), ref);
+    return ref;
+  }
+
+  dereference(name: Symbol) {
+    return this.#references.get(name.toString()).deref();
   }
 
   send(subject: Recipient, message: Message) {
