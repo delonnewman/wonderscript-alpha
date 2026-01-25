aa0d167f8ac04b4a63b54c5bbedd2563d971cc69
Delon R. Newman <contact@delonnewman.name>
2026-01-24 19:10:39 -0500

    Add AlienDispatch

--- src/wonderscript/kernel/Alien.ts
+++ src/wonderscript/kernel/Alien.ts
@@ -1,14 +1,38 @@
+import { Dispatch, Recipient } from "./Dispatch";
+import { Message } from "./Message";
 import { Symbol } from "./Symbol";
 
-export class Alien extends Symbol {
-    readonly #object: Object;
+export class Alien extends Symbol implements Recipient {
+  readonly dispatch: Dispatch;
 
-    constructor(object: Object, name: string, namespace?: string) {
-        super(name, namespace);
-        this.#object = object;
+  constructor(object: Object, name: string, namespace?: string) {
+    super(name, namespace);
+    this.dispatch = new AlienDispatch(object);
+  }
+}
+
+export class AlienDispatch extends Dispatch {
+  #object: Object;
+
+  constructor(object: Object) {
+    super();
+    this.#object = object;
+  }
+
+  lookup(msg: Message): Function {
+    let method = this.#object[msg.toString()];
+    if (method === undefined) {
+      method = this.#object[msg.name];
     }
 
-    get dispatch() {
-        // return DyanmicJSDispatch instance
+    if (typeof method !== "function") {
+      throw new Error(`Don't understand ${msg}`);
     }
+
+    return method;
+  }
+
+  addMethod(msg: Message, method: Function) {
+    this.#object[msg.toString()] = method;
+  }
 }
