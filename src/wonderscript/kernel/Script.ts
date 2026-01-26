3e130a2f615dcbf54a724749de2211e4acfa6118
Delon R. Newman <contact@delonnewman.name>
2026-01-26 14:01:54 -0500

    Remove createReference and dereference

--- src/wonderscript/kernel/Script.ts
+++ src/wonderscript/kernel/Script.ts
@@ -1,31 +1,17 @@
 import { Message } from "./Message";
 import { Recipient } from "./Dispatch";
-import { Ref } from "./Ref";
-import { Symbol } from "./Symbol";
 import { Transmission } from "./Transmission";
 import { Continuation } from "./Continuation";
 
 export class Script {
   #transmissions: Continuation;
-  #references: Map<string, Ref>;
 
   constructor() {
     this.#transmissions = new Continuation();
-    this.#references = new Map();
   }
 
   get transmissions() {
     return this.#transmissions.toArray();
-  }
-
-  createReference(name: Symbol, value?: unknown) {
-    const ref = new Ref(value);
-    this.#references.set(name.toString(), ref);
-    return ref;
-  }
-
-  dereference(name: Symbol) {
-    return this.#references.get(name.toString()).deref();
   }
 
   send(subject: Recipient, message: Message) {
@@ -34,6 +20,7 @@ export class Script {
   }
 
   execute() {
+    // TODO: how will we pass the current continuation when requested?
     for (const transmission of this.transmissions) {
       transmission.execute();
     }
