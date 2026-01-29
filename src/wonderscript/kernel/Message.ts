c4f70118755746790c4eb32e7b96823099c2b0ad
Delon R. Newman <contact@delonnewman.name>
2026-01-28 20:06:22 -0500

    Remove Continuation

--- src/wonderscript/kernel/Message.ts
+++ src/wonderscript/kernel/Message.ts
@@ -1,31 +1,23 @@
 import { Symbol } from "./Symbol";
-import { Continuation } from "./Continuation";
 
 export class Message {
   readonly name: Symbol;
   readonly args?: Readonly<unknown[]>;
-  readonly continuation?: Continuation;
 
   static intern(
     value: string,
-    options: { continue?: Continuation; args?: Readonly<unknown[]> } = {}
+    options: { args?: Readonly<unknown[]> } = {}
   ) {
     const symbol = Symbol.intern(value);
-    return new this(symbol, options.continue, options.args);
+    return new this(symbol, options.args);
   }
 
   constructor(
     name: Symbol,
-    continuation?: Continuation,
     args?: Readonly<unknown[]>
   ) {
     this.name = name;
     this.args ??= args;
-    this.continuation ??= continuation;
-  }
-
-  isQuery() {
-    return this.continuation !== undefined;
   }
 
   isUnary() {
