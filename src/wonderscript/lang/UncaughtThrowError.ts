56383600759293df7382de9e135a8dccdb602cf3
Delon Newman <contact@delonnewman.name>
2026-09-14 16:06:50 -0400

    Get stack from nested errors

--- src/wonderscript/lang/UncaughtThrowError.ts
+++ src/wonderscript/lang/UncaughtThrowError.ts
@@ -2,8 +2,12 @@ import { Context } from "./Context";
 
 export class UncaughtThrowError extends Error {
   constructor(nested: Error | string, scope?: Context) {
-    const message = (nested as Error).message ?? nested;
-    super(message as string);
+    if (nested instanceof Error) {
+      super(nested.message);
+      this.stack = nested.stack;
+    } else {
+      super(nested);
+    }
     if (scope) {
       const first = `${this.message}\n${scope.stacktrace().toString()}`;
       this.stack = this.stack?.replace(this.message, first);
