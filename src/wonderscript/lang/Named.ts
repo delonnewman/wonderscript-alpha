e7926de92c33eb8ecd290406c3933fde0934ccdc
Delon Newman <contact@delonnewman.name>
2026-09-21 12:41:04 -0400

    Add name and namespace functions

--- src/wonderscript/lang/Named.ts
+++ src/wonderscript/lang/Named.ts
@@ -1,9 +1,9 @@
 import { Nil } from "./Nil";
 
-export interface Named<Name = string> {
+export interface Named<Name extends string = string> {
   name: Name;
   namespace: string | Nil;
-  hasNamespace(): boolean;
+  hasNamespace(): boolean; // TODO: remove
 }
 
 export function namedHash<Name extends string = string>(
@@ -15,4 +15,26 @@ export function namedHash<Name extends string = string>(
   }
 
   return name;
+}
+
+export function hasNamespace<Name extends string = string>(
+  name: Named<Name>
+): boolean {
+  return name.namespace !== null;
+}
+
+export function name(named: string | Named): string {
+  if (typeof named === "string") {
+    return named;
+  }
+
+  return named.name;
+}
+
+export function namespace(named: string | Named): string | undefined {
+  if (typeof named === "string") {
+    return undefined;
+  }
+
+  return named.namespace;
 }
