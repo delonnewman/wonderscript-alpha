3f23e794869efe0a0323379f7eb7f24d9f89a1b7
Delon Newman <contact@delonnewman.name>
2026-09-21 14:48:04 -0400

    Implement toJS for JSTypeMessage

--- src/wonderscript/lang/javascript.ts
+++ src/wonderscript/lang/javascript.ts
@@ -1,12 +1,14 @@
-import { BaseMessage } from "./Message/BaseMessage";
 import { UnaryMessage } from "./Message/UnaryMessage";
+import { Context } from "./Context";
+import { Form } from "../compiler/core";
+import { emit } from "../compiler/emit";
 
 export class JSTypeMessage extends UnaryMessage {
   sendTo(obj: Record<string, unknown>): unknown {
     return typeof obj;
   }
 
-  toJS(): string {
-    return "string";
+  toJS(ctx: Context, obj: Form): string {
+    return `typeof ${emit(obj, ctx)}`;
   }
 }
