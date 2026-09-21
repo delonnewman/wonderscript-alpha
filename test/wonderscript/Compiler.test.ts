2865cb30c6305802fc894613da6920ea3bef56f1
Delon Newman <contact@delonnewman.name>
2026-09-21 14:42:55 -0400

    Fix compiler test

--- test/wonderscript/Compiler.test.ts
+++ test/wonderscript/Compiler.test.ts
@@ -56,7 +56,7 @@ describe("Compiler", () => {
     });
 
     test("send message that is passed as a function argument", () => {
-      const forms = "((fn* (msg) (send '(1 2 3) msg)) :js/toString)";
+      const forms = "((fn* (msg) (send '(1 2 3) msg)) js/call nil :js/toString)";
       const output = subject.evalString(forms);
       expect(output).toEqual("1,2,3");
     });
