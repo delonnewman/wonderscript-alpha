1a801661625755e87bb3efd520bbc235ab8cf6e6
Delon Newman <contact@delonnewman.name>
2026-09-25 06:00:26 -0400

    Use dynamic example tests

--- test/wonderscript/reader/syntaxQuoteReader.test.ts
+++ test/wonderscript/reader/syntaxQuoteReader.test.ts
@@ -6,72 +6,57 @@ import {
 } from "../../../src/wonderscript/reader/syntaxQuoteReader";
 import { Symbol } from "../../../src/wonderscript/lang/Symbol";
 import { SEND_SYM } from "../../../src/wonderscript/compiler/emit/emitSend";
-import { p } from "../../../src/wonderscript/util";
 import { Keyword } from "../../../src/wonderscript/lang/Keyword";
+import { prStr } from "../../../src/wonderscript/compiler";
 
 describe("syntaxQuoteReader", () => {
-  test("trivial quoting", () => {
-    const { form: form } = readString("`(a 2 3)")[0];
-    expect(form).toEqual([ARRAY_SYM, [QUOTE_SYM, Symbol.intern("a")], 2, 3]);
-  });
-  test("unquoting", () => {
-    const { form: form } = readString("`(1 ~x 3)")[0];
-    expect(form).toEqual([ARRAY_SYM, 1, Symbol.intern("x"), 3]);
-  });
-  test("unquoting", () => {
-    const { form: form } = readString("`(1 ~x ~y)")[0];
-    expect(form).toEqual([
-      ARRAY_SYM,
-      1,
-      Symbol.intern("x"),
-      Symbol.intern("y"),
-    ]);
-  });
-  test("unquoting and spliced unquoting together", () => {
-    const { form: actual } = readString("`(1 ~x ~@ys)")[0];
-
-    const expected = [
-      SEND_SYM,
-      [ARRAY_SYM, 1, Symbol.intern("x")],
-      [Keyword.intern("concat", "js"), Symbol.intern("ys"), [ARRAY_SYM]],
-    ];
-    p(actual);
-    p(expected);
-    expect(actual).toEqual(expected);
-  });
-  test("unquoting and spliced unquoting together", () => {
-    const { form: actual } = readString("`(a ~x 4 ~@ys 5)")[0];
-
-    const expected = [
-      SEND_SYM,
-      [ARRAY_SYM, [QUOTE_SYM, Symbol.intern("a")], Symbol.intern("x"), 4],
-      [Keyword.intern("concat", "js"), Symbol.intern("ys"), [ARRAY_SYM, 5]],
-    ];
-    expect(actual).toEqual(expected);
-  });
-  test("spliced unquoting with 3 forms", () => {
-    const { form: actual } = readString("`(1 ~@x 3)")[0];
-
-    const expected = [
-      SEND_SYM,
-      [ARRAY_SYM, 1],
-      [Keyword.intern("concat", "js"), Symbol.intern("x"), [ARRAY_SYM, 3]],
-    ];
-    expect(actual).toEqual(expected);
-  });
-  test("spliced unquoting with 4 forms", () => {
-    const { form: actual } = readString("`(1 ~@x 3 4)")[0];
-
-    const expected = [
-      SEND_SYM,
-      [ARRAY_SYM, 1],
+  const examples: [string, unknown][] = [
+    ["`(a 2 3)", [ARRAY_SYM, [QUOTE_SYM, Symbol.intern("a")], 2, 3]],
+    ["`(1 ~x 3)", [ARRAY_SYM, 1, Symbol.intern("x"), 3]],
+    ["`(1 ~x ~y)", [ARRAY_SYM, 1, Symbol.intern("x"), Symbol.intern("y")]],
+    [
+      "`(1 ~x ~@ys)",
       [
-        Keyword.intern("concat", "js"),
-        Symbol.intern("x"),
-        [ARRAY_SYM, 3],
-        [ARRAY_SYM, 4],
+        SEND_SYM,
+        [ARRAY_SYM, 1, Symbol.intern("x")],
+        [Keyword.intern("concat", "js"), Symbol.intern("ys"), [ARRAY_SYM]],
       ],
-    ];
-    expect(actual).toEqual(expected);
+    ],
+    [
+      "`(a ~x 4 ~@ys 5)",
+      [
+        SEND_SYM,
+        [ARRAY_SYM, [QUOTE_SYM, Symbol.intern("a")], Symbol.intern("x"), 4],
+        [Keyword.intern("concat", "js"), Symbol.intern("ys"), [ARRAY_SYM, 5]],
+      ],
+    ],
+    [
+      "`(1 ~@xs 3)",
+      [
+        SEND_SYM,
+        [ARRAY_SYM, 1],
+        [Keyword.intern("concat", "js"), Symbol.intern("xs"), [ARRAY_SYM, 3]],
+      ],
+    ],
+    [
+      "`(1 ~@xs 3 4)",
+      [
+        SEND_SYM,
+        [ARRAY_SYM, 1],
+        [
+          Keyword.intern("concat", "js"),
+          Symbol.intern("xs"),
+          [ARRAY_SYM, 3],
+          [ARRAY_SYM, 4],
+        ],
+      ],
+    ],
+  ];
+
+  examples.forEach(([input, expected]) => {
+    test(`${input} => ${prStr(expected)}`, () => {
+      const { form: actual } = readString(input)[0];
+      expect(actual).toEqual(expected);
+    });
   });
 });
