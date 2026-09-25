5b45b5c33e7989cc64a64cffe144b5fa69c1ea81
Delon Newman <contact@delonnewman.name>
2026-09-25 06:03:56 -0400

    Move readString to reader.ts

--- src/wonderscript/reader.ts
+++ src/wonderscript/reader.ts
@@ -1,3 +1,23 @@
+import { EOF, Form, isEOF } from "./compiler/core";
+import { PushBackReader } from "./reader/PushBackReader";
+import { read } from "./reader/read";
+
+export { PushBackReader } from './reader/PushBackReader';
 export { read } from "./reader/read";
-export { PushBackReader } from "./reader/PushBackReader";
-export * from './reader/readString';
+
+export type ReadForm = {
+  form: Form;
+  line: number;
+  column: number;
+};
+
+export function readString(s: string): ReadForm[] {
+  const r = new PushBackReader(s);
+  const forms: ReadForm[] = [];
+
+  while (true) {
+    let form = read(r, { eofIsError: false, eofValue: EOF });
+    if (isEOF(form)) return forms;
+    if (form != null) forms.push({ form, line: r.line, column: r.column });
+  }
+}
