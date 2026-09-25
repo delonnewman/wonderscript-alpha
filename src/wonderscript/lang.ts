10c45c25ad136013d311bf4889ce8e5551bafd98
Delon Newman <contact@delonnewman.name>
2026-09-24 22:33:02 -0400

    Remove new classes

--- src/wonderscript/lang.ts
+++ src/wonderscript/lang.ts
@@ -5,19 +5,6 @@ import { Vector } from "./lang/Vector";
 import { Definition } from "./lang/Definition";
 import { Package } from "./lang/Package";
 import { Message } from "./lang/Message";
-import { ObjectPool } from "./lang/Object";
-import { Class } from "./lang/Class";
-import {
-  False,
-  FalseClass,
-  Float,
-  Nil,
-  NilClass,
-  PrimitiveType,
-  True,
-  TrueClass,
-  String,
-} from "./lang/primitive";
 
 export * from "./lang/Meta";
 export * from "./lang/Named";
@@ -33,7 +20,6 @@ export * from "./lang/Definition";
 export * from "./lang/Message";
 export * from "./lang/Class";
 export * from "./lang/Object";
-export * from "./lang/primitive";
 
 globalThis.wonderscript ??= {};
 globalThis.wonderscript.lang = {
@@ -44,15 +30,4 @@ globalThis.wonderscript.lang = {
   Module: Package,
   Definition,
   Message,
-  Class,
-  ObjectPool,
-  PrimitiveType,
-  Nil,
-  NilClass,
-  True,
-  TrueClass,
-  False,
-  FalseClass,
-  String,
-  Float,
 };
